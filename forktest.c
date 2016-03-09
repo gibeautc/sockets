7a30d3b10fc45dd682cfa4eaacb62b2b397295b7
Administrator <geomail240@hotmail.com>
2016-03-08 20:21:38 -0800

    added forktest.c

---
+++ forktest.c
@@ -0,0 +1,27 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+main()
+{
+	pid_t spawnpid = -5;
+	int ten = 10;
+	spawnpid = fork();
+	switch (spawnpid)
+	{
+		case -1:
+			perror("Hull Breach!");
+			exit(1);
+			break;
+		case 0:
+			ten = ten + 1;
+			printf("I am the child! ten = %d\n", ten);
+			break;
+		default :
+			ten=ten-1;
+			printf("I am the parent! ten = %d\n", ten);
+			break;
+	}
+	printf("This will be executed by both of us!\n");
+}
